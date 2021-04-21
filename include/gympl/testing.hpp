1623735854219c040718fd64a6e8d3e62c01a060
gyzkard <gyzkard@gyzkard.com>
2021-04-21 18:10:08 +0200

    add isEqual with epsilon

--- include/gympl/testing.hpp
+++ include/gympl/testing.hpp
@@ -3,10 +3,24 @@
 #define INCLUDED_GYMPL_TESTING
 
 #include <cassert>
+#include <type_traits>
+#include <cmath>
 
 #include <gympl/macroTools.hpp>
 
 #define GYMPL_REQUIRE(expr) (void)((! ! (expr)) || (assert(expr), std::exit(EXIT_FAILURE), 0))
+
+namespace gympl
+{
+    namespace testing
+    {
+        template <typename T>
+        inline bool isEqual(T v1, T v2, T epsilon)
+        {
+            return std::abs(v1 - v2) <= epsilon;
+        }
+    }
+}
 
 #define GYMPL_PRINTDEFINE(x) #x ": " GYMPL_TOSTR2(x)
 
